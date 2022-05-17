8d5d246fa9952fff39c8b3de3d2c36b3cf2a6b59
armand-vs <armandvs@telesure.co.za>
2022-05-17 16:23:30 +0200

    add user update endpoint

--- controllers/profile.js
+++ controllers/profile.js
@@ -11,6 +11,25 @@ const handleProfileGet = (req, res, db) => {
     .catch(err => res.status(400).json('error getting user'))
 }
 
+const handleProfileUpdate = (req, res, db) => {
+  const {id} = req.params;
+  const {name, age, pet} = req.body.formInput;
+
+  db('users')
+    .where({id})
+    .update({name})
+    .then(res=> {
+      if(res){
+        res.json('success')
+      }
+      else {
+        res.status(400).json('Unable to update')
+      }
+    })
+    .catch(err => res.status(400).json('Error updating user'))
+}
+
 module.exports = {
-  handleProfileGet
+  handleProfileGet,
+  handleProfileUpdate
 }
